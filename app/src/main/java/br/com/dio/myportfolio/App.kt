cf25bcf2d1d596dff80d0002c0be0db175eda6cc
Thierry Boiago <thierry123123@gmail.com>
2021-10-29 21:24:42 -0300

    Projeto Base

---
+++ app/src/main/java/br/com/dio/myportfolio/App.kt
@@ -0,0 +1,15 @@
+package br.com.dio.myportfolio
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+        }
+    }
+}
--- app/src/main/java/br/com/dio/myportfolio/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/myportfolio/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.dio.myportfolio
+package br.com.dio.myportfolio.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.dio.myportfolio.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
